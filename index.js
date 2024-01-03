6ee662956877293bd30ac4f579198f20b7b3797b
Daniel <jesus.cs.7.2010@gmail.com>
2024-01-03 11:43:29 -0500

    Finished

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dns = require('dns');
+let contador = 0
+const diccionarioUrls = {}
 // Basic Configuration
+app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -18,6 +21,40 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.url
+  const caracterSeparador = '/'
+  const indiceSeparador = originalUrl.indexOf(caracterSeparador)
+  let soloUrl
+
+  if (indiceSeparador !== -1 && originalUrl[indiceSeparador+1] === caracterSeparador) {
+    soloUrl = originalUrl.substring(indiceSeparador + 2)
+
+    dns.lookup(soloUrl, (err, address, family) => {
+      if (err) {
+        console.error(err);
+        res.json({ error: 'invalid url'})
+        return;
+      }
+      contador++
+      diccionarioUrls[contador] = originalUrl
+      res.json({ original_url: originalUrl, short_url: contador})
+    });
+  } else {
+    res.json({ error: 'invalid url'})
+  }
+  
+})
+
+app.get('/api/shorturl/:idUrl', (req, res) => {
+  const obtenerUrl = diccionarioUrls[req.params.idUrl]
+  res.redirect(obtenerUrl)
+})
+
+app.get('/test', (req, res) => {
+  res.redirect('https://www.facebook.com')
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
